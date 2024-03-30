a4647c96455bfad2bb4a100c16ab0498ad387835
Beilong-Tang <bt132@duke.edu>
2024-03-30 14:31:27 -0400

    add Exception and ViewablePng base class

---
+++ src/main/java/oogasalad/view/ViewablePng.java
@@ -0,0 +1,13 @@
+package oogasalad.view;
+
+/**
+ * Wrapper for choosing an png image for an object
+ */
+public class ViewablePng {
+
+  public ViewablePng(String location){
+    
+  }
+
+
+}
---
+++ src/main/java/oogasalad/view/exception/FileDoesNotExistException.java
@@ -0,0 +1,5 @@
+package oogasalad.view.exception;
+
+public class FileDoesNotExistException {
+
+}
