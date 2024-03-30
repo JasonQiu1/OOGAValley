9a343f3ca26934fe6d3dcbe0edcbec3253705fa8
Beilong-Tang <bt132@duke.edu>
2024-03-30 16:37:30 -0400

    add simple viewablePng class

--- src/main/java/oogasalad/view/ViewablePng.java
+++ src/main/java/oogasalad/view/ViewablePng.java
@@ -1,13 +1,33 @@
 package oogasalad.view;
 
+import javafx.scene.image.Image;
+import oogasalad.view.exception.FileDoesNotExistException;
+import oogasalad.view.exception.FileNotPngException;
+
 /**
- * Wrapper for choosing an png image for an object
+ * Wrapper for choosing a png image for an object
  */
 public class ViewablePng {
 
-  public ViewablePng(String location){
-    
+  private final Image image;
+  public ViewablePng(String url) throws FileNotPngException, FileDoesNotExistException{
+    if (url==null){
+      throw new FileDoesNotExistException("file does not exist");
+    }
+    if (!checkValidFile(url)){
+      throw new FileNotPngException("File type not png");
+    }
+    image = new Image(url);
   }
+
+  public Image getImage(){
+    return image;
+  }
+
+  private boolean checkValidFile(String url){
+    return url.endsWith(".png");
+  }
+
 
 
 }
