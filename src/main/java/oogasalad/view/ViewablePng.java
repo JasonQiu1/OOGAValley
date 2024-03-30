b4000ac3b75febf256812808d4daf07ec175382b
Beilong-Tang <bt132@duke.edu>
2024-03-30 17:28:25 -0400

    add viewablePng and Test

--- src/main/java/oogasalad/view/ViewablePng.java
+++ src/main/java/oogasalad/view/ViewablePng.java
@@ -1,7 +1,10 @@
 package oogasalad.view;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import javafx.scene.image.Image;
-import oogasalad.view.exception.FileDoesNotExistException;
 import oogasalad.view.exception.FileNotPngException;
 
 /**
@@ -10,24 +13,56 @@ import oogasalad.view.exception.FileNotPngException;
 public class ViewablePng {
 
   private final Image image;
-  public ViewablePng(String url) throws FileNotPngException, FileDoesNotExistException{
-    if (url==null){
-      throw new FileDoesNotExistException("file does not exist");
-    }
-    if (!checkValidFile(url)){
-      throw new FileNotPngException("File type not png");
-    }
-    image = new Image(url);
+
+  /**
+   * Construct a viewable png class
+   *
+   * @param url the location of the png file
+   * @throws FileNotPngException
+   * @throws FileNotFoundException
+   */
+  public ViewablePng(String url)
+      throws FileNotPngException, FileNotFoundException {
+    checkUrl(url);
+    InputStream inputStream = new FileInputStream(url);
+    image = new Image(inputStream);
   }
 
-  public Image getImage(){
+  /**
+   * Construct a viewable png class with the specified width and height
+   *
+   * @param url    the location of the png file
+   * @param width  the width the image should be resized to
+   * @param height the height the image should be resized to
+   * @throws FileNotPngException
+   * @throws FileNotFoundException
+   */
+  public ViewablePng(String url, double width, double height)
+      throws FileNotPngException, FileNotFoundException {
+    checkUrl(url);
+    InputStream inputStream = new FileInputStream(url);
+    image = new Image(inputStream, width, height, true, true);
+  }
+
+  /**
+   * Get the javafx image class
+   *
+   * @return javafx image class
+   */
+  public Image getImage() {
     return image;
   }
 
-  private boolean checkValidFile(String url){
-    return url.endsWith(".png");
+
+  private void checkUrl(String url) throws FileNotFoundException, FileNotPngException {
+    if (url == null) {
+      throw new FileNotFoundException("file url cannot be null");
+    }
+    File f = new File(url);
+    if (!f.exists()) {
+      throw new FileNotFoundException("file not found");
+    } else if (!url.endsWith(".png")) {
+      throw new FileNotPngException("File type not png");
+    }
   }
-
-
-
 }
