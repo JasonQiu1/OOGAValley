b1ac30c83b5830314d62eadb009902d07323a601
Ryan Teachman <rlt42@duke.edu>
2024-04-03 02:37:39 -0400

    added tile highlighting for when the mouse is over the tile in map editor and added that right clicking removes the tile

--- src/main/java/oogasalad/view/editor/MapEditor/Cell.java
+++ src/main/java/oogasalad/view/editor/MapEditor/Cell.java
@@ -1,14 +1,15 @@
 package oogasalad.view.editor.MapEditor;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Cell extends StackPane {
-    BuildableMap map;
+    ImageView image;
     Rectangle base;
-    final int HEIGHT = 37;
-    final int WIDTH = 37;
+    final int HEIGHT = 37; //read from file
+    final int WIDTH = 50;
 
     public Cell(TileSelector ts) {
         super();
@@ -19,10 +20,31 @@ public class Cell extends StackPane {
         super.getChildren().add(base);
         setOnMouseClicked(event -> {
             if(ts.getLastTileSelected() != null){
-                ImageView formattedImage = this.format(ts.getLastTileSelected().getImage());
-                super.getChildren().add(formattedImage);
+                super.getChildren().remove(image);
+                if(event.getButton() == MouseButton.SECONDARY){
+                    image = null;
+                }
+                else{
+                    ImageView formattedImage = this.format(ts.getLastTileSelected().getImage());
+                    image = formattedImage;
+                    super.getChildren().add(formattedImage);
+                }
             }
         });
+
+        setOnMouseEntered(event -> {
+            base.setFill(Color.GRAY);
+            if(image != null){
+                image.setOpacity(.5);
+            }
+        });
+        setOnMouseExited(event -> {
+            base.setFill(Color.WHITE);
+            if(image != null){
+                image.setOpacity(1);
+            }
+        });
+
 
     }
 
