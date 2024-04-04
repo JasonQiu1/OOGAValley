f15f80ea0deee9caa6b807036fd07998712a394c
Spencer Katz <sek49@duke.edu>
2024-04-04 10:39:22 -0400

    Deleted Unwanted Class

--- src/main/java/oogasalad/Game/GameModel/Land.java
+++ src/main/java/oogasalad/Game/GameModel/Land.java
@@ -1,15 +1,16 @@
 package oogasalad.Game.GameModel;
 
 import java.util.Properties;
+import oogasalad.Game.GameModel.Properties.GameObjectProperties;
 import oogasalad.Game.GameModel.Properties.LandProperties;
 
 public class Land extends GameObject implements Plantable {
 
   private final boolean isPlantable;
 
-  private final LandProperties properties;
+  private final GameObjectProperties properties;
 
-  public Land(String id, int startState, LandProperties properties, boolean isPlantable) {
+  public Land(String id, int startState, GameObjectProperties properties, boolean isPlantable) {
     super(id, startState, properties);
     this.isPlantable = isPlantable;
     this.properties = properties;
--- src/main/java/oogasalad/Game/GameModel/Structure.java
+++ src/main/java/oogasalad/Game/GameModel/Structure.java
@@ -1,10 +1,11 @@
 package oogasalad.Game.GameModel;
 
+import oogasalad.Game.GameModel.Properties.GameObjectProperties;
 import oogasalad.Game.GameModel.Properties.StructureProperties;
 
 public class Structure extends GameObject {
 
-  public Structure(String id, int startState, StructureProperties properties) {
+  public Structure(String id, int startState, GameObjectProperties properties) {
     super(id, startState, properties);
   }
 
