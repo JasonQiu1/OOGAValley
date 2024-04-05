71f528a4fcc68abf98ac57ce69bd82c0e1fc2163
Beilong-Tang <bt132@duke.edu>
2024-04-05 11:48:50 -0400

    add game time and test

---
+++ src/test/java/oogasalad/GameTimeTest.java
@@ -0,0 +1,36 @@
+package oogasalad;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import oogasalad.Game.GameModel.GameTime;
+import oogasalad.Game.GameModel.GameTimeInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTimeTest {
+
+  private GameTime gameTime;
+
+  @BeforeEach
+  void setUp() {
+    gameTime = new GameTime(1, 0, 0);
+  }
+
+  @Test
+  void testIfTheGameTimeHandlesDifference() {
+    for (int i = 0; i < 100; i++) {
+      gameTime.update();
+    }
+    assertEquals(gameTime.getMinute(), 0);
+  }
+
+  @Test
+  void testDifferenceGameTime() {
+    GameTimeInterface gameTime1 = new GameTime(1, 8, 0);
+    GameTimeInterface gameTime2 = new GameTime(2, 10, 30);
+    assertEquals(gameTime1.getDifferenceInMinutes(gameTime2), 1590);
+    assertEquals(gameTime2.getDifferenceInMinutes(gameTime1), -1590);
+  }
+
+
+}
