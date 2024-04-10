5267e064cab0d263a3d92eee5ee53160b4a10b4f
Jason Qiu <jq48@duke.edu>
2024-04-10 16:18:28 -0400

    feat [49-integrate-properties]: added constructor to GameObjectProperties and subclasses

--- src/main/java/oogasalad/Game/GameModel/PropertiesOfGameObjects/CollectableProperties.java
+++ src/main/java/oogasalad/Game/GameModel/PropertiesOfGameObjects/CollectableProperties.java
@@ -1,8 +1,14 @@
 package oogasalad.Game.GameModel.PropertiesOfGameObjects;
 
+import oogasalad.Game.GameModel.ReadOnlyProperties;
+
 public class CollectableProperties extends GameObjectProperties {
   private String itemOnCollection;
   private int quantityOnCollection;
+
+  CollectableProperties(ReadOnlyProperties properties) {
+    super(properties);
+  }
 
   public String getItemOnCollection() {
     return itemOnCollection;
--- src/main/java/oogasalad/Game/GameModel/PropertiesOfGameObjects/GameObjectProperties.java
+++ src/main/java/oogasalad/Game/GameModel/PropertiesOfGameObjects/GameObjectProperties.java
@@ -1,12 +1,20 @@
 package oogasalad.Game.GameModel.PropertiesOfGameObjects;
 
-import java.util.Map;
 import oogasalad.Game.GameModel.GameTime;
 import oogasalad.Game.GameModel.Item;
 import oogasalad.Game.GameModel.ReadOnlyProperties;
 
 public abstract class GameObjectProperties {
+
   private ReadOnlyProperties properties;
+
+  protected GameObjectProperties(ReadOnlyProperties properties) {
+    this.properties = properties;
+  }
+
+  protected void setProperties(ReadOnlyProperties properties) {
+    this.properties = properties;
+  }
 
   public long modifiedTimeToUpdate(GameTime gameTime) {
     return properties.getInteger("updateTime");
