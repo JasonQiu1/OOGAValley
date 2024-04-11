befdb2a58003599f831ed20b720637b747cb7d4b
Ryan Teachman <rlt42@duke.edu>
2024-04-11 01:40:30 -0400

    added save button class

---
+++ src/main/java/oogasalad/view/editor/MapEditor/SaveButton.java
@@ -0,0 +1,10 @@
+package oogasalad.view.editor.MapEditor;
+
+import javafx.scene.control.Button;
+
+public class SaveButton extends Button {
+    public SaveButton(){
+        super("Save");
+        setOnMouseClicked(e -> );
+    }
+}
