c52401573fb9f70d4eab664f51377f6696e3ddf0
Beilong-Tang <bt132@duke.edu>
2024-04-22 16:52:05 -0400

    testing gameobject

--- src/test/java/oogasalad/model/StructureTest.java
+++ src/test/java/oogasalad/model/StructureTest.java
@@ -1,6 +1,7 @@
 package oogasalad.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -10,12 +11,9 @@ import oogasalad.model.data.GameConfigurablesStore;
 import oogasalad.model.data.GameConfiguration;
 import oogasalad.model.data.GameState;
 import oogasalad.model.data.Properties;
-import oogasalad.model.gameObjectFactories.GameObjectFactory;
-import oogasalad.model.gameobject.GameObject;
-import oogasalad.model.gameobject.Land;
+import oogasalad.model.gameobject.Item;
 import oogasalad.model.gameobject.Structure;
 import oogasalad.model.gameplay.GameTime;
-import oogasalad.model.gameplay.GameWorld;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -37,14 +35,19 @@ public class StructureTest {
     property.getProperties().put("updatable", "true");
     property.getProperties().put("updateTime", "10");
     property.getProperties().put("expirable", "false");
-
+    property.getProperties().put("updateTransformation", "tilled_grass");
     Map<String, String> drops = new HashMap<>();
     drops.put("seed", "2");
     property.getMapProperties().put("dropsOnDestruction", drops);
+    Map<String, String> interactTransform = new HashMap<>();
+    interactTransform.put("validItem", "tilled_grass");
+    property.getMapProperties().put("interactTransformations", interactTransform);
     allEditableConfigurables.put(id, property);
     GameState gameState = new GameState();
     testingStructure = new Structure(id, gameState.getEditableGameTime().copy());
+    gameConfiguration.save(fileName);
   }
+
 
   @Test
   public void testImagePathCorrect() {
@@ -57,7 +60,48 @@ public class StructureTest {
   }
 
   @Test
+  public void testStructureInteractionInvalid() {
+    assertFalse(testingStructure.interactionValid(new Item("thing")));
+  }
+
+  @Test
+  public void testStructureInteractionValid() {
+    String id = "tilled_grass";
+    GameConfigurablesStore editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
+    Map<String, Properties> allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
+    Properties property = new Properties();
+    allEditableConfigurables.put(id, property);
+    assertTrue(testingStructure.interactionValid(new Item("validItem")));
+  }
+
+  @Test
   public void retrieveStructureDropsAmountCorrectly() {
     assertEquals(2, testingStructure.getItemsOnDestruction().get("seed"));
+  }
+
+  @Test
+  public void verifyId() {
+    assertEquals("grass_structure", testingStructure.getId());
+  }
+
+  @Test
+  public void updateNotEnoughTimeWillNotUpdate() {
+    testingStructure.update(new GameTime(1, 1, 1));
+    testingStructure.update(new GameTime(1, 1, 1));
+    assertEquals("grass_structure", testingStructure.getId());
+  }
+
+  @Test
+  public void updateShouldHaveUpdated() {
+    String id = "tilled_grass";
+    GameConfigurablesStore editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
+    Map<String, Properties> allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
+    Properties property = new Properties();
+    property.getProperties().put("type", "Structure");
+    property.getProperties().put("expirable", "false");
+    allEditableConfigurables.put(id, property);
+    testingStructure.update(new GameTime(2, 2, 2));
+    testingStructure.update(new GameTime(2, 2, 2));
+    assertEquals("tilled_grass", testingStructure.getId());
   }
 }
