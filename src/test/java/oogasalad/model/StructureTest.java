f6a7677d97869a2cff5ee7744689a9f62910c55f
Spencer Katz <sek49@duke.edu>
2024-04-21 21:05:20 -0400

    Small structure test update

--- src/test/java/oogasalad/model/StructureTest.java
+++ src/test/java/oogasalad/model/StructureTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import oogasalad.model.data.GameConfigurablesStore;
+import oogasalad.model.data.GameConfiguration;
+import oogasalad.model.data.Properties;
 import oogasalad.model.gameObjectFactories.GameObjectFactory;
 import oogasalad.model.gameobject.Structure;
 import oogasalad.model.gameplay.GameTime;
@@ -18,10 +22,16 @@ public class StructureTest {
   public void setUp() throws IOException {
     GameObjectFactory g1 = new GameObjectFactory();
     GameTime time = new GameTime(0,0,0);
-//    ReadOnlyGameConfigurablesStore store = GameConfiguration.getConfigurablesStore();
-//    Map<String, ReadOnlyProperties> properties = store.getAllConfigurables();
-//    ReadOnlyProperties prop1 = Properties.of("templates/Structure.json");
-    testingStructure = (Structure) g1.createNewGameObject("Wheat", time, new HashMap<>());
+    GameConfiguration gameConfiguration = GameConfiguration.of("test");
+    GameConfigurablesStore editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
+    Map<String, Properties> allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
+    Properties properties = new Properties();
+    System.out.println(allEditableConfigurables);
+    properties.getProperties().put("wheat", "100");
+    allEditableConfigurables.put("store", properties);
+    editableConfigurablesStore.save("test.json");
+    gameConfiguration.save("test.json");
+    testingStructure
   }
 
   @Test
