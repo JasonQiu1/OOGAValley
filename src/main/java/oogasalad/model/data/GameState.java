f424cdce69929141063380f2fdd7cee82c1641d5
Spencer Katz <sek49@duke.edu>
2024-04-22 16:29:08 -0400

    New Structure Tests added

--- src/main/java/oogasalad/model/data/GameState.java
+++ src/main/java/oogasalad/model/data/GameState.java
@@ -138,8 +138,7 @@ public class GameState implements ReadOnlyGameState {
   }
 
   public GameTime getEditableGameTime() {
-    // TODO: IMPLEMENT
-    return null;
+    return new GameTime(1,1,1);
   }
 
   private static final DataFactory<GameState> FACTORY = new DataFactory<>(GameState.class);
--- src/test/java/oogasalad/model/DataGeneration.java
+++ src/test/java/oogasalad/model/DataGeneration.java
@@ -1,34 +1,29 @@
 package oogasalad.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import oogasalad.model.data.GameConfigurablesStore;
 import oogasalad.model.data.GameConfiguration;
 import oogasalad.model.data.GameState;
 import oogasalad.model.data.Properties;
-import oogasalad.model.gameObjectFactories.GameObjectFactory;
 import oogasalad.model.gameobject.GameObject;
 import oogasalad.model.gameobject.Land;
-import oogasalad.model.gameobject.Structure;
+import oogasalad.model.gameplay.Game;
 import oogasalad.model.gameplay.GameTime;
 import oogasalad.model.gameplay.GameWorld;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class StructureTest {
+public class DataGeneration {
 
-  private Structure testingStructure;
-
-  @BeforeEach
-  public void setUp() throws IOException {
-    //    The file to save the gameWorld to
+  /**
+   * Create a default game with grasses as the tiles 10 x 15 with grass
+   */
+  @Test
+  void generateGameWorld() throws IOException {
+//    The file to save the gameWorld to
     String fileName = "testWorld1.json";
 //    the id for the grass land
-    String id = "grass_structure";
+    String id = "grass_land";
     GameConfiguration gameConfiguration = GameConfiguration.of("TempGameConfiguration.json");
     GameConfigurablesStore editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
     Map<String, Properties> allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
@@ -37,27 +32,26 @@ public class StructureTest {
     property.getProperties().put("updatable", "true");
     property.getProperties().put("updateTime", "10");
     property.getProperties().put("expirable", "false");
-
-    Map<String, String> drops = new HashMap<>();
-    drops.put("seed", "2");
-    property.getMapProperties().put("dropsOnDestruction", drops);
     allEditableConfigurables.put(id, property);
     GameState gameState = new GameState();
-    testingStructure = new Structure(id, gameState.getEditableGameTime().copy());
+    GameWorld gameWorld = gameState.getEditableGameWorld();
+    GameObject land = new Land(id, gameState.getEditableGameTime().copy());
+    //   horizontal -x, vertical  - y
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 10; j++) {
+        gameWorld.setTileGameObject(land, i, j, 0);
+      }
+    }
+    gameWorld.update(new GameTime(1,1,1));
+    editableConfigurablesStore.save(fileName);
+    gameState.save(fileName);
   }
 
   @Test
-  public void testImagePathCorrect() {
-    assertEquals("/img/grass.jpg", testingStructure.getImagePath());
+  void testGeneratedWorld() {
+//    the code for the previous test
+    String fileName = "testWorld1.json";
+    
   }
 
-  @Test
-  public void retrieveStructureDropsCorrectly() {
-    assertTrue(testingStructure.getItemsOnDestruction().containsKey("seed"));
-  }
-
-  @Test
-  public void retrieveStructureDropsAmountCorrectly() {
-    assertEquals(2, testingStructure.getItemsOnDestruction().get("seed"));
-  }
 }
