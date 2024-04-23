c0f393b0c2a3c5556f66baaf7e0b1aefb9ad8a50
Spencer Katz <sek49@duke.edu>
2024-04-23 18:29:45 -0400

    Created Constructor to start tile tests

--- src/test/java/oogasalad/model/TileTest.java
+++ src/test/java/oogasalad/model/TileTest.java
@@ -2,6 +2,7 @@ package oogasalad.model;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import oogasalad.model.data.Properties;
@@ -24,10 +25,35 @@ public class TileTest extends BaseGameObjectTest {
   private Properties testingStructureProperties;
   private Properties testingLandProperties;
   private Properties testingCollectableProperties;
+  private Tile tileToTest;
 
   @Override
   protected void initializeGameObjects() throws IOException {
+    testingStructureProperties = Properties.of("test/testingTileStructure.json");
+    testingLandProperties = Properties.of("test/testingTileLand.json");
+    testingCollectableProperties = Properties.of("test/testingTileCollectable.json");
+    addPropertiesToStoreWithAlreadyCreatedProperties
+        (testingStructureProperties.getString("name"), testingStructureProperties);
+    addPropertiesToStoreWithAlreadyCreatedProperties
+        (testingLandProperties.getString("name"), testingLandProperties);
+    addPropertiesToStoreWithAlreadyCreatedProperties
+        (testingCollectableProperties.getString("name"), testingCollectableProperties);
 
+    tileStructure = (Structure) getFactory()
+        .createNewGameObject(testingStructureProperties.getString("name"),
+            new GameTime(1,1,1), new HashMap<>());
+    tileLand = (Land) getFactory()
+        .createNewGameObject(testingLandProperties.getString("name"),
+            new GameTime(1,1,1), new HashMap<>());
+    Map<String, Integer> collectableItems = new HashMap<>();
+    collectableItems.put("axe", 2);
+    tileCollectable = (Collectable) getFactory()
+        .createNewGameObject(testingCollectableProperties.getString("name"),
+            new GameTime(1,1,1), collectableItems);
+    tileToTest = new Tile();
+    tileToTest.setCollectable(tileCollectable);
+    tileToTest.setLand(tileLand);
+    tileToTest.setStructure(tileStructure);
   }
 }
 
