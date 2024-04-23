ac66a4d23877da9ed028e96e13589808d03be306
Spencer Katz <sek49@duke.edu>
2024-04-23 17:40:54 -0400

    Refactored Collectable tests

--- src/test/java/oogasalad/model/CollectableTest.java
+++ src/test/java/oogasalad/model/CollectableTest.java
@@ -9,8 +9,8 @@ import java.util.HashMap;
 import java.util.Map;
 import oogasalad.model.data.GameConfigurablesStore;
 import oogasalad.model.data.GameConfiguration;
-import oogasalad.model.data.GameState;
 import oogasalad.model.data.Properties;
+import oogasalad.model.gameObjectFactories.GameObjectFactory;
 import oogasalad.model.gameobject.Collectable;
 import oogasalad.model.gameobject.Item;
 import oogasalad.model.gameobject.Land;
@@ -20,33 +20,24 @@ import org.junit.jupiter.api.Test;
 
 public class CollectableTest {
   private Collectable testingCollectable;
+  private Properties testingCollectableProperties;
+  private GameConfigurablesStore editableConfigurablesStore;
+  private Map<String, Properties> allEditableConfigurables;
 
   @BeforeEach
   public void setUp() throws IOException {
-    //    The file to save the gameWorld to
     String fileName = "testWorld1.json";
-//    the id for the grass land
-    String id = "grass_land";
     GameConfiguration gameConfiguration = GameConfiguration.of("TempGameConfiguration.json");
-    GameConfigurablesStore editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
-    Map<String, Properties> allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
-    Properties property = new Properties();
-    property.getProperties().put("image", "/img/grass.jpg");
-    property.getProperties().put("updatable", "true");
-    property.getProperties().put("updateTime", "10");
-    property.getProperties().put("expirable", "false");
-    property.getProperties().put("updateTransformation", "tilled_grass");
-    Map<String, String> seeds = new HashMap<>();
-    seeds.put("wheat_seed", "wheat");
-    property.getMapProperties().put("plantableSeeds", seeds);
-    Map<String, String> interactTransform = new HashMap<>();
-    interactTransform.put("validItem", "tilled_grass");
-    property.getMapProperties().put("interactTransformations", interactTransform);
-    allEditableConfigurables.put(id, property);
-    GameState gameState = new GameState();
+    editableConfigurablesStore = GameConfiguration.getEditableConfigurablesStore();
+    allEditableConfigurables = editableConfigurablesStore.getAllEditableConfigurables();
+    testingCollectableProperties = Properties.of("test/testingGrassCollectable.json");
+    editableConfigurablesStore.getAllEditableConfigurables().put("grass_collectable",
+        testingCollectableProperties);
+    allEditableConfigurables.put("grass_collectable", testingCollectableProperties);
+    GameObjectFactory factory = new GameObjectFactory();
     Map<String, Integer> itemsOnCollection = new HashMap<>();
     itemsOnCollection.put("hoe", 2);
-    testingCollectable = new Collectable(id, new GameTime(1,1,1), itemsOnCollection);
+    testingCollectable = (Collectable) factory.createNewGameObject("grass_collectable", new GameTime(1,1,1), itemsOnCollection);
     gameConfiguration.save(fileName);
   }
 
