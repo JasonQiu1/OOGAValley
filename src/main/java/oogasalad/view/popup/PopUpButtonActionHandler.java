50874353837c082bbc33e5f6818846914ceef0e7
Yinuo Guo <yg202@duke.edu>
2024-04-23 22:58:04 -0400

    refactor pop up

---
+++ src/main/java/oogasalad/view/popup/PopUpButtonActionHandler.java
@@ -0,0 +1,32 @@
+package oogasalad.view.popup;
+
+import java.util.function.Consumer;
+import javafx.scene.layout.StackPane;
+
+public class PopUpButtonActionHandler {
+
+  private final Consumer<Boolean> choiceCallback;
+  private final StackPane parentStackPane;
+  private final StackPane popUpStackPane;
+
+  public PopUpButtonActionHandler(Consumer<Boolean> choiceCallback, StackPane parentStackPane,
+      StackPane popUpStackPane) {
+    this.choiceCallback = choiceCallback;
+    this.parentStackPane = parentStackPane;
+    this.popUpStackPane = popUpStackPane;
+  }
+
+  public void yesAction() {
+    choiceCallback.accept(true);
+    closePopup();
+  }
+
+  public void noAction() {
+    choiceCallback.accept(false);
+    closePopup();
+  }
+
+  private void closePopup() {
+    parentStackPane.getChildren().remove(popUpStackPane);
+  }
+}
--- src/main/java/oogasalad/view/popup/PopUpStackPane.java
+++ src/main/java/oogasalad/view/popup/PopUpStackPane.java
@@ -1,7 +1,13 @@
 package oogasalad.view.popup;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 import java.util.function.Consumer;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,110 +19,109 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import oogasalad.view.popup.buttons.NoButton;
-import oogasalad.view.popup.buttons.YesButton;
 
-/**
- * This class is a StackPane that creates a pop-up window with a message and two buttons. The
- * buttons are YesButton and NoButton. The pop-up window is added to the parentStackPane. The
- * choiceCallback is a Consumer that takes a boolean as an argument. The boolean is true if the user
- * clicks the YesButton and false if the user clicks the NoButton.
- */
+
 public class PopUpStackPane extends StackPane {
 
-  private final String popUpMessage;
-  private final YesButton yesButton;
-  private final NoButton noButton;
-  private final Consumer<Boolean> choiceCallback;
-  private final StackPane parentStackPane;
+  private final static double centerBackgroundWidth = 300;
+  private final static double centerBackgroundHeight = 200;
+  private final static String centerBackgroundImage = "img/pop-up/pop-up-background.png";
+  private final static String backgroundImagePath = "img/pop-up/bg-shadow.png";
+  private final String csvFilePath;
+  private final VBox vBox = new VBox(10);
+  private final HBox buttonBox = new HBox(10);
+  private final PopUpButtonActionHandler actionHandler;
+  private final ResourceBundle resourceBundle;
+  private Label messageLabel;
+  private ImageView centerBackgroundImageView;
 
-  /**
-   * Constructor for PopUpStackPane
-   *
-   * @param popUpMessage    the message to be displayed in the pop-up window
-   * @param yesButtonText   the text to be displayed on the YesButton
-   * @param noButtonText    the text to be displayed on the NoButton
-   * @param parentStackPane the StackPane to which the pop-up window will be added
-   * @param choiceCallback  the Consumer that takes a boolean as an argument
-   */
-  public PopUpStackPane(String popUpMessage, String yesButtonText, String noButtonText,
-      StackPane parentStackPane,
-      Consumer<Boolean> choiceCallback) {
+  public PopUpStackPane(ResourceBundle resourceBundle, StackPane parentStackPane,
+      Consumer<Boolean> choiceCallback,
+      String csvFilePath) {
     super();
-    this.popUpMessage = popUpMessage;
-    this.yesButton = new YesButton(yesButtonText);
-    this.noButton = new NoButton(noButtonText);
-    this.parentStackPane = parentStackPane;
-    this.choiceCallback = choiceCallback;
+    this.resourceBundle = resourceBundle;
+    this.actionHandler = new PopUpButtonActionHandler(choiceCallback, parentStackPane, this);
+    this.csvFilePath = csvFilePath;
     initialize();
   }
 
   private void initialize() {
+    readAndCreateButtonsFromFile(csvFilePath);
+    createMessageLabel();
+    createVBox();
+    createCenterBackgroundImageView();
     setBackground(createBackground());
-    setUpContent();
-    setUpButton();
+    setAlignment(Pos.CENTER);
+    getChildren().addAll(centerBackgroundImageView, vBox);
   }
 
   private Background createBackground() {
-    Image backgroundImage = new Image("img/pop-up/bg-shadow.png");
+    Image backgroundImage = new Image(backgroundImagePath);
     BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT,
         BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
     return new Background(background);
   }
 
-  private void setUpContent() {
-    StackPane centerStackPane = createCenterStackPane();
-    getChildren().addAll(centerStackPane);
+
+  private void createCenterBackgroundImageView() {
+    centerBackgroundImageView =
+        new ImageView(new Image(centerBackgroundImage));
+    centerBackgroundImageView.setFitWidth(centerBackgroundWidth);
+    centerBackgroundImageView.setFitHeight(centerBackgroundHeight);
   }
 
-  private StackPane createCenterStackPane() {
-    StackPane centerStackPane = new StackPane();
-    centerStackPane.setPrefSize(Utils.centerWidth, Utils.centerHeight);
-    centerStackPane.getChildren().addAll(createCenterBackgroundImageView(), createVBox());
-    return centerStackPane;
-  }
-
-  private ImageView createCenterBackgroundImageView() {
-    Image centerBackgroundImage = new Image("img/pop-up/pop-up-background.png");
-    ImageView centerBackgroundImageView = new ImageView(centerBackgroundImage);
-    centerBackgroundImageView.setFitWidth(Utils.centerWidth);
-    centerBackgroundImageView.setFitHeight(Utils.centerHeight);
-    return centerBackgroundImageView;
-  }
-
-  private VBox createVBox() {
-    VBox vBox = new VBox(Utils.padding);
-    vBox.getChildren().addAll(createMessageLabel(), createButtonBox());
-    vBox.setAlignment(Pos.CENTER);
-    return vBox;
-  }
-
-  private Label createMessageLabel() {
-    Label messageLabel = new Label(popUpMessage);
-    messageLabel.getStyleClass().add("pop-up-label");
-    return messageLabel;
-  }
-
-  private HBox createButtonBox() {
-    HBox buttonBox = new HBox(Utils.padding);
-    buttonBox.getChildren().addAll(yesButton, noButton);
+  private void createVBox() {
+    vBox.getChildren().addAll(messageLabel, buttonBox);
     buttonBox.setAlignment(Pos.CENTER);
-    return buttonBox;
+    messageLabel.setAlignment(Pos.CENTER);
+    vBox.setAlignment(Pos.CENTER);
   }
 
-  private void setUpButton() {
-    yesButton.setOnAction(event -> {
-      choiceCallback.accept(true);
-      closePopup();
-    });
-
-    noButton.setOnAction(event -> {
-      choiceCallback.accept(false);
-      closePopup();
-    });
+  private void createMessageLabel() {
+    messageLabel = new Label(resourceBundle.getString("are_you_sure"));
+    messageLabel.getStyleClass().add("pop-up-label");
   }
 
-  private void closePopup() {
-    parentStackPane.getChildren().remove(this);
+  private void readAndCreateButtonsFromFile(String filePath) {
+    try (Scanner scanner = new Scanner(new File(filePath))) {
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        Button button = createButtonFromCSV(line);
+        addToButtonBox(button);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
+
+  private Button createButtonFromCSV(String csvLine) {
+    String[] parts = csvLine.split(",");
+    String buttonId = parts[0];
+    String buttonText = parts[1];
+    String methodName = parts[2];
+
+    try {
+      Method method = PopUpButtonActionHandler.class.getMethod(methodName);
+      Button button = new Button(resourceBundle.getString(buttonText));
+      button.setId(buttonId);
+
+      button.setOnAction(event -> {
+        try {
+          method.invoke(actionHandler);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      });
+      return button;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+
+  private void addToButtonBox(Button button) {
+    buttonBox.getChildren().add(button);
+  }
+
 }
