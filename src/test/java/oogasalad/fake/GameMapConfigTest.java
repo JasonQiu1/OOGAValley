c939f8269e374f18a3ec075057df210f0efb909d
Yinuo Guo <yg202@duke.edu>
2024-04-26 11:51:30 -0400

    generate map

--- src/test/java/oogasalad/fake/GameMapConfigTest.java
+++ src/test/java/oogasalad/fake/GameMapConfigTest.java
@@ -6,15 +6,35 @@ import oogasalad.fake.api.exception.SaveNotValidException;
 import oogasalad.fake.config.GameConfig;
 import oogasalad.fake.map.Coord;
 import oogasalad.fake.map.GameMap;
+import oogasalad.fake.object.Land;
+import oogasalad.fake.object.Plant;
 import org.junit.jupiter.api.Test;
 
 public class GameMapConfigTest {
 
   String path = "valley_01/save.farm";
+
   @Test
   void testLoadAndSaveConfig() throws IOException, SaveNotValidException {
     GameConfig gameConfig = new GameConfig(path);
     GameMap gameMap = new GameMap(path);
+    gameMap.save();
+  }
+
+  @Test
+  void testCreateGameMap() throws IOException, SaveNotValidException {
+    GameConfig gameConfig = new GameConfig(path);
+    HashMap<Coord, Land> landPositionMap = new HashMap<>();
+    HashMap<Coord, Plant> plantPositionMap = new HashMap<>();
+    GameMap gameMap = new GameMap(10, 15, landPositionMap, plantPositionMap, path);
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 10; j++) {
+        landPositionMap.put(new Coord(i, j),
+            new Land(gameConfig.getLandConfigMap().get("grass_land")));
+        plantPositionMap.put(new Coord(i, j),
+            new Plant(gameConfig.getPlantConfigMap().get("wheat"), null));
+      }
+    }
     gameMap.save();
   }
 
