5cb1463fd8318a8e54f66258c3c2faa407b3b880
SAAD HAKIM <SH604@duke.edu>
2024-04-27 15:06:42 -0400

    created new class LoaderListDisplay

---
+++ src/main/java/oogasalad/view/start/LoaderListDisplay.java
@@ -0,0 +1,64 @@
+package oogasalad.view.start;
+
+import java.io.File;
+import java.util.Optional;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoaderListDisplay {
+
+  private static final Logger LOG = LogManager.getLogger(LoaderListDisplay.class);
+  File selectedFile;
+  private final String myTitle;
+  private final String defaultDirectoryPath;
+  private final VBox vBox;
+
+  public LoaderListDisplay(String title, String defaultFolderPath) {
+    vBox = new VBox();
+    myTitle = title;
+    defaultDirectoryPath = defaultFolderPath;
+    vBox.setId(title);
+  }
+
+  public Optional<File> open(Stage stage) {
+    stage.setTitle(myTitle);
+
+    ListView<String> listView = new ListView<>();
+    File directory = new File(defaultDirectoryPath);
+    File[] files = directory.listFiles();
+
+    if (files != null) {
+      for (File file : files) {
+        listView.getItems().add(file.getName());
+      }
+    }
+
+    Button selectButton = new Button("Load");
+    selectButton.setOnAction(event -> selectFile(listView));
+
+    VBox vBox = new VBox(listView, selectButton);
+    Scene scene = new Scene(vBox, 400, 300);
+
+    stage.setScene(scene);
+    stage.showAndWait();
+
+    return Optional.ofNullable(selectedFile);
+  }
+
+  private void selectFile(ListView<String> listView) {
+    String selectedFileName = listView.getSelectionModel().getSelectedItem();
+    if (selectedFileName != null) {
+      File pickedFile = new File(defaultDirectoryPath + "/" + selectedFileName);
+      selectedFile = pickedFile;
+
+      Stage stage = (Stage) listView.getScene().getWindow();
+      stage.close();
+    }
+  }
+
+}
