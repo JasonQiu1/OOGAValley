b087b95a0c92610c49af04313f1bf572a285362a
Jason Qiu <jq48@duke.edu>
2024-04-28 13:21:27 -0400

    test [113-game-tests]: full method coverage on Game

--- src/test/java/oogasalad/model/gameplay/GameTest.java
+++ src/test/java/oogasalad/model/gameplay/GameTest.java
@@ -1,12 +1,16 @@
 package oogasalad.model.gameplay;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
+import oogasalad.model.api.ReadOnlyItem;
 import oogasalad.model.data.GameConfiguration;
 import oogasalad.model.data.Properties;
+import oogasalad.model.gameobject.Item;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -24,8 +28,7 @@ class GameTest {
     assertEquals("COLLECTGOALTEST", configuration.getRules().getString("configName"));
 
     Game game = new Game(configuration);
-    assertEquals("COLLECTGOALTEST",
-        game.getGameConfiguration().getRules().getString("configName"));
+    assertEquals("COLLECTGOALTEST", game.getGameConfiguration().getRules().getString("configName"));
     assertFalse(game.isGameOver());
     ((Bag) game.getGameState().getBag()).addItems(
         configuration.getRules().getStringIntegerMap("collectGoal"));
@@ -45,5 +48,23 @@ class GameTest {
       ((GameTime) game.getGameState().getGameTime()).advanceTo(0, 0);
     }
     assertTrue(game.isGameOver());
+  }
+
+  @Test
+  void shopping() {
+    Game game = new Game();
+    Shop editableShop = (Shop) game.getGameState().getShop();
+    ReadOnlyItem pickaxe = new Item("Pickaxe");
+    editableShop.setPossibleItems(List.of(pickaxe));
+    editableShop.forceItemRotation(new GameTime(0, 0, 0));
+
+    assertFalse(game.buyItem("Pickaxe"));
+    ((GameState) game.getGameState()).addMoney(editableShop.getItems().get(pickaxe).intValue());
+    assertTrue(game.buyItem("Pickaxe"));
+    assertTrue(game.getGameState().getBag().getItems().containsKey(pickaxe));
+    assertEquals(0, game.getGameState().getMoney());
+
+    assertDoesNotThrow(() -> game.sellItem("Pickaxe"));
+    assertEquals(pickaxe.getWorth(), game.getGameState().getMoney());
   }
 }
